b3fd877a203fca718c83babaafc5e4787339eed2
d8cq6m7ywb-commits <d8cq6m7ywb@privaterelay.appleid.com>
2025-11-21 12:08:35 -0800

    Update fit-entry.js

--- fit-entry.js
+++ fit-entry.js
@@ -1,10 +1,8 @@
 // fit-entry.js
-// Minimal browser entry for fit-file-parser
+// Browserify entry that exposes FitParser on window for index.html
 
 const FitParserModule = require("fit-file-parser");
-
-// Handle both CommonJS and default export
 const FitParser = FitParserModule.default || FitParserModule;
 
-// Expose constructor on window so index.html can use `window.FitParser`
+// Expose constructor globally
 window.FitParser = FitParser;
