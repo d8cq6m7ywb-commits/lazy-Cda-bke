c88ba3335f67fe67c16652382a65ee75b3ad46e5
d8cq6m7ywb-commits <d8cq6m7ywb@privaterelay.appleid.com>
2025-11-21 11:26:28 -0800

    Add files via upload

--- bundle.js
+++ bundle.js
@@ -1,9 +1,13 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 // fit-entry.js
+// Minimal browser entry for fit-file-parser
+
 const FitParserModule = require("fit-file-parser");
+
+// Handle both CommonJS and default export
 const FitParser = FitParserModule.default || FitParserModule;
 
-// expose constructor globally for the browser code in index.html
+// Expose constructor on window so index.html can use `window.FitParser`
 window.FitParser = FitParser;
 },{"fit-file-parser":5}],2:[function(require,module,exports){
 'use strict'
