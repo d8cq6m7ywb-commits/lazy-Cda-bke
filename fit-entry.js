ec98fb16316e9ed6521c9ff3bbe63155a1ce4efe
d8cq6m7ywb-commits <d8cq6m7ywb@privaterelay.appleid.com>
2025-11-21 12:32:36 -0800

    Update fit-entry.js

--- fit-entry.js
+++ fit-entry.js
@@ -1,8 +1,21 @@
 // fit-entry.js
-// Browserify entry that exposes FitParser on window for index.html
+// Browserify entry that exposes the FitParser constructor on window
+// so index.html can use it.
 
 const FitParserModule = require("fit-file-parser");
-const FitParser = FitParserModule.default || FitParserModule;
 
-// Expose constructor globally
-window.FitParser = FitParser;
+// According to the npm docs, the constructor is the default export.
+// Fallbacks are defensive in case of different bundling.
+const FitParser =
+  (FitParserModule && FitParserModule.default) ||
+  (FitParserModule && FitParserModule.FitParser) ||
+  FitParserModule;
+
+// Expose to the browser
+if (typeof window !== "undefined") {
+  window.FitParser = FitParser;
+
+  // Also mirror the older UMD-style global just in case
+  window.FIT = window.FIT || {};
+  window.FIT.FitParser = FitParser;
+}
