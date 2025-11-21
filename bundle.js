15e51134f3e845c54390a843c1dd35bdb448211f
d8cq6m7ywb-commits <d8cq6m7ywb@privaterelay.appleid.com>
2025-11-21 12:33:24 -0800

    Add files via upload

--- bundle.js
+++ bundle.js
@@ -1,12 +1,25 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 // fit-entry.js
-// Browserify entry that exposes FitParser on window for index.html
+// Browserify entry that exposes the FitParser constructor on window
+// so index.html can use it.
 
 const FitParserModule = require("fit-file-parser");
-const FitParser = FitParserModule.default || FitParserModule;
 
-// Expose constructor globally
-window.FitParser = FitParser;
+// According to the npm docs, the constructor is the default export.
+// Fallbacks are defensive in case of different bundling.
+const FitParser =
+  (FitParserModule && FitParserModule.default) ||
+  (FitParserModule && FitParserModule.FitParser) ||
+  FitParserModule;
+
+// Expose to the browser
+if (typeof window !== "undefined") {
+  window.FitParser = FitParser;
+
+  // Also mirror the older UMD-style global just in case
+  window.FIT = window.FIT || {};
+  window.FIT.FitParser = FitParser;
+}
 },{"fit-file-parser":5}],2:[function(require,module,exports){
 'use strict'
 
